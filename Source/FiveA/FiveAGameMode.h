fc15534c1caf3a7d395e37e6daff7a766e33f9f1
yauhei <yauhei0505@gmail.com>
2025-03-16 23:33:27 +0800

    初始项目
    Signed-off-by: yauhei <yauhei0505@gmail.com>

---
+++ Source/FiveA/FiveAGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FiveAGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AFiveAGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AFiveAGameMode();
+};
+
+
+
